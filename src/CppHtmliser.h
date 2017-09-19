f87fcdd93192919820f6e85cdb2f1d7ab7b20307
wetrolam <wetrolam@gmail.com>
2017-09-19 10:11:15 +0200

    htmlise comments, keywords and literals

--- src/CppHtmliser.h
+++ src/CppHtmliser.h
@@ -3,6 +3,7 @@
 
 #include "Parser.h"
 #include <fstream>
+#include <map>
 
 inline std::string encode(char letter)
 {
@@ -16,6 +17,31 @@ inline std::string encode(char letter)
     }
 }
 
+// TODO temporary function
+inline std::string getStyle()
+{
+    return
+        "        <style>\n"
+        "            .keyword {\n"
+        "                font-weight: bold;\n"
+        "            }\n"
+        "            .comment {\n"
+        "                color: green;\n"
+        "                /*color: gray;*/\n"
+	"                font-style: italic;\n"
+        "            }\n"
+        "            .preprocessor {\n"
+        "                /*color: blue;*/\n"
+        "                color: #800000;\n"
+        "                font-weight: bold;\n"
+        "            }\n"
+        "            .literal {\n"
+        "                /*color: #800000;*/\n"
+        "                color: blue;\n"
+        "            }\n"
+        "        </style>\n";
+}
+
 // TODO: move to another file
 inline std::string getHtmlBeginning(std::string title)
 {
@@ -23,6 +49,7 @@ inline std::string getHtmlBeginning(std::string title)
         "<html>\n"
         "    <head>\n"
         "        <title> " + title + " </title>\n"
+        "" + getStyle() + ""
         "    </head>\n"
         "    <body>\n"
         ;
@@ -37,18 +64,66 @@ inline std::string getHtmlEnding()
         ;
 }
 
+// TODO temporary function
+inline std::string toHtmlClass(CXTokenKind kind) {
+     switch(kind) {
+        case CXToken_Punctuation:
+            return "punctuation";
+        case CXToken_Keyword:
+            return "keyword";
+        case CXToken_Identifier:
+            return "identifier";
+        case CXToken_Literal:
+            return "literal";
+        case CXToken_Comment:
+            return "comment";
+        default:
+            return "unknown";
+    }
+}
+
 inline std::string cppToHtml(std::string cppFilePath)
 {
+    // parse source file
+
+    Parser parser(cppFilePath);
+
+    std::map<unsigned, CXTokenKind> beginning;
+    std::map<unsigned, CXTokenKind> ending;
+
+    for(const Token & t : parser.getTokens()) {
+        beginning[t.location.offset] = t.kind;
+        ending[t.location.offset + t.length] = t.kind;
+    }
+
+    // write html file
+
     std::string htmlFilePath = cppFilePath + ".html";
     std::ifstream cpp(cppFilePath);
     std::ofstream html(htmlFilePath);
 
     html << getHtmlBeginning(cppFilePath);
     html << std::endl << "<pre><code>";
+
+    unsigned offset = 0;
     char letter;
     while(cpp.get(letter)) {
+        std::map<unsigned, CXTokenKind>::iterator it;
+        it = ending.find(offset);
+        if(it != ending.end() && (it->second == CXToken_Comment || it->second == CXToken_Keyword || it->second == CXToken_Literal) ) { // TODO change the condition (this is working version)
+            html << "</span>";
+        }
+
+        it = beginning.find(offset);
+        if(it != beginning.end() && (it->second == CXToken_Comment || it->second == CXToken_Keyword || it->second == CXToken_Literal) ) { // TODO change the condition (this is working version)
+            html << "<span class=\"" << toHtmlClass(it->second) << "\">";
+        }
+
         html << encode(letter);
+
+        offset ++;
     }
+
     html << "</code><pre>" << std::endl;
     html << getHtmlEnding();
 
